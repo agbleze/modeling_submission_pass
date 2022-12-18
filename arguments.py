dfd172536374e9da78a6ab2703d8f3bf4d99d0c2
agbleze <lagbleze@st.ug.edu.gh>
2022-12-18 23:09:44 +0100

    Create arguments.py

---
+++ arguments.py
@@ -0,0 +1,18 @@
+from argparse import Namespace
+
+
+args = Namespace(
+    target_variable = 'state',
+    target_variable_transformed = 'state_category',
+    predictor_variables = ['average_answer_time_in_hours', 'progress_percent', 'test_variable_transform'],
+    numeric_features = ['progress_percent', 'average_answer_time_in_min', 'extra_time_min',
+                        ],
+    categorical_features = ['work_rate'],
+    binary_feature = ['test_variable_transform'],
+    predictors = ['progress_percent', 'average_answer_time_in_min', 'extra_time_min',
+                  'work_rate', 'test_variable_transform'],
+    selected_predictors = ['progress_percent', 'extra_time_min', 'work_rate',
+                        ],
+    selected_numeric_features = ['progress_percent', 'extra_time_min']
+
+)
