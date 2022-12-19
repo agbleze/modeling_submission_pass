0b26dd3805adbcf8d6b8ca6c3960ed349dfde3f0
agbleze <lagbleze@st.ug.edu.gh>
2022-12-19 15:04:44 +0100

    Update data_split.py

--- data/data_split.py
+++ data/data_split.py
@@ -2,8 +2,6 @@ from sklearn.model_selection import train_test_split
 import pandas as pd
 
 
-
-
 def split_data(data: pd.DataFrame, features: list, target: str, 
                test_size: int = 0.3, random_state=2022):
     if isinstance(features, str):
