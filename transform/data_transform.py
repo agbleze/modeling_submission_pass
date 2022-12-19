989864cfb03583887f3f419451eb78b4e01894c2
agbleze <lagbleze@st.ug.edu.gh>
2022-12-19 14:17:39 +0100

    Update data_transform.py

--- transform/data_transform.py
+++ transform/data_transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from arguments import args
 
 rescale_negative_to_zero =  lambda x: 0 if x < 0 else x
 convert_hours_to_minute = lambda x: x * 60
@@ -13,7 +14,7 @@ class DataTransformer(object):
         
     def convert_hours_to_minute(self, column_to_convert_minutes: str = 'average_answer_time_in_hours'):
         self.data_copied = self.data.copy()
-        new_columnname = column_to_convert_minutes[:-3] + 'min'
+        new_columnname = column_to_convert_minutes[:-5] + 'min'
         self.data_copied[new_columnname] = self.data_copied[column_to_convert_minutes].apply(convert_hours_to_minute)
         return self.data_copied
             
@@ -21,15 +22,15 @@ class DataTransformer(object):
 
     # function to create work_rate variable
     @classmethod
-    def define_work_rate(self, data_to_transform: pd.DataFrame = None, 
+    def define_work_rate(self, data_to_transform: pd.DataFrame, # = None, 
                         min_time_alloted: str = 'min_task_time_in_min', 
                         max_time_alloted: str = 'max_task_time_in_min',
                         actual_time_used: str = 'average_answer_time_in_min'
                         ):
-        if data_to_transform is None:           
-            self.data_to_transform = self.data_copied
-        else:
-            self.data_to_transform = data_to_transform
+        # if data_to_transform is None:           
+        #     self.data_to_transform = self.data_copied
+        # else:
+        self.data_to_transform = data_to_transform
         
         self.data_to_transform['work_rate'] = (np.where((self.data_to_transform[actual_time_used] < 
                                                          self.data_to_transform[min_time_alloted]
@@ -57,14 +58,15 @@ class DataTransformer(object):
 
 # compute extra time
     @classmethod
-    def calculate_extra_time_min(self, data_to_transform: pd.DataFrame = None, max_time_min="max_task_time_in_min", 
+    def calculate_extra_time_min(self, data_to_transform: pd.DataFrame, # = None, 
+                                 max_time_min="max_task_time_in_min", 
                                 actual_time_min="average_answer_time_in_min",
                                 make_negative_time_zero_minutes: callable = rescale_negative_to_zero
                                 ):
-        if data_to_transform is None:
-            self.data_to_transform = self.data_copied
-        else:
-            self.data_to_transform = data_to_transform
+        # if data_to_transform is None:
+        #     self.data_to_transform = self.data_copied
+        # else:
+        self.data_to_transform = data_to_transform
             
         self.data_to_transform['time_diff'] = self.data_to_transform[actual_time_min] - self.data_to_transform[max_time_min]
         
@@ -74,22 +76,31 @@ class DataTransformer(object):
     
     
     @classmethod
-    def convert_categorical_to_numeric(self, data: pd.DataFrame = None,
+    def convert_categorical_to_numeric(self, data: pd.DataFrame, # = None,
                                        column_to_convert: str = 'test_variable', 
                                        string_value_pair: dict = {'a': 0, 'b': 1}
                                        ):
-        if data is None:
-            self.data_to_transform = self.data_copied
+        # if data is None:
+        #     self.data_to_transform = self.data_copied
         self.data_to_transform = data
         new_columnname = column_to_convert + '_transform'
         self.data_to_transform[new_columnname] = self.data_to_transform[column_to_convert].apply(transform_test_variable)
         return self.data_to_transform
+    
+    
+    @classmethod
+    def transform_target_variable(cls, data: pd.DataFrame,
+                                  target_variable: str = args.target_variable,                                  
+                                  bin_values: str = args.not_passed,
+                                  transform_column_name: str = 'state_category'
+                                  ):
+        if bin_values:
+            transform_state_variable = lambda x: 0 if x in bin_values else 1
+            data[transform_column_name] = data[target_variable].apply(transform_state_variable)
+            return data
+            
         
         
-
-
-
-
 
 
 
