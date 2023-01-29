31777a494dbc06879b7224a962e646c38a0d1f0e
agbleze <lagbleze@st.ug.edu.gh>
2023-01-29 13:57:57 +0100

    added test fixtures for data

--- test_data.py
+++ test_data.py
@@ -2,7 +2,9 @@ import pytest
 from utils.get_path import get_data_path
 import pandas as pd
 from data.data_read import get_data_from_numbers_file
-from data.data_split import train_test_split
+from data.data_split import split_data
+from arguments import args
+from transform.data_transform import DataTransformer
 
 
 datapath = get_data_path(folder_name='DS_Practical', file_name='all_merge_df.csv')
@@ -15,12 +17,49 @@ def dataset():
     df = pd.read_csv(datapath)
     return df
 
+
+@pytest.fixture()
+def data_transformer():
+    data_transformer_obj = DataTransformer(data=dataset)
+    return data_transformer_obj
+
+
+@pytest.fixture()
+def transformed_hours_to_minute_data():
+    data_transformed = data_transformer.convert_hours_to_minute()
+    return data_transformed
+    
+
+@pytest.fixture()
+def train_test_data(dataset):
+    data_split = split_data(data=dataset,
+                            features=args.selected_predictors,
+                            target=args.target_variable_transformed
+                            )
+    return data_split
+     
+     
+     
 @pytest.fixture()
 def model():
     pass
 
 
 ##
+
+#TODO:
+    # TEST define work rate for work rate column
+    # Test define work rate column for values slow, normal, fast
+    
+    
+def test_work_rate_column_available():
+    pass
+
+
+def test_work_rate_values():
+    pass
+
+
 
 def test_get_data_from_numbers_file():
     data_path = get_data_path(folder_name='DS_Practical',
