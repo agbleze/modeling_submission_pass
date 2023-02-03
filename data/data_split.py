bfb9f8e43d49419d86632b243c56a3bf547114ea
agbleze <lagbleze@st.ug.edu.gh>
2023-02-03 23:16:06 +0100

    Update data_split.py

--- data/data_split.py
+++ data/data_split.py
@@ -1,9 +1,17 @@
 from sklearn.model_selection import train_test_split
 import pandas as pd
+from collections import namedtuple
 
-
-def split_data(data: pd.DataFrame, features: list, target: str, 
-               test_size: int = 0.3, random_state=2022):
+train_test_data = namedtuple(typename='train_test_data',
+                             field_names=['X_train','X_test',
+                                          'y_train','y_test'
+                                          ]
+                             )
+def split_data(data: pd.DataFrame, 
+               features: list, 
+               target: str, 
+               test_size: int = 0.3, 
+               random_state=2022):
     if isinstance(features, str):
         features = [features]
     
@@ -12,7 +20,12 @@ def split_data(data: pd.DataFrame, features: list, target: str,
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, 
                                                         random_state=random_state
                                                         )
-    return X_train, X_test, y_train, y_test
+    return train_test_data(X_train=X_train, 
+                           X_test=X_test,
+                           y_train=y_train,
+                           y_test=y_test
+                           )
+
     
         
     
